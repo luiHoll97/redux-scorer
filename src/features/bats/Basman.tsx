2deba41c64aa83aeceb64c39fe2790874120d0da
luiHoll97 <tech.luiholl@gmail.com>
2023-07-18 19:10:17 +0100

    second batsman added

--- src/features/bats/Basman.tsx
+++ src/features/bats/Basman.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { addBatsmanName, addRunsToBatsman, selectBat } from "./batsmanSlice";
+import { addBatsmanName, addRunsToBatsman, selectBat, selectBat2, changeStrike } from "./batsmanSlice";
 import styles from "./Batsman.module.css";
 
 export const Batsman = () => {
     const batsman = useAppSelector(selectBat);
+    const batsman2 = useAppSelector(selectBat2);
     const dispatch = useAppDispatch();
     const [batsmanName, setBatsmanName] = useState("");
     const [runs, setRuns] = useState(0);
@@ -16,6 +17,9 @@ export const Batsman = () => {
 
     const addRuns = () => {
         dispatch(addRunsToBatsman(runs));
+        if (runs % 2 !== 0) {
+            dispatch(changeStrike());
+        }
         setRuns(0);
     };
 
@@ -55,13 +59,24 @@ export const Batsman = () => {
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
                     <td>{batsman.name}</td>
                     <td>{batsman.runs}</td>
                     <td>{batsman.balls}</td>
                     <td>{batsman.fours}</td>
                     <td>{batsman.sixes}</td>
                     <td>{((batsman.runs / batsman.balls) *100).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                    <td>{batsman2.name}</td>
+                    <td>{batsman2.runs}</td>
+                    <td>{batsman2.balls}</td>
+                    <td>{batsman2.fours}</td>
+                    <td>{batsman2.sixes}</td>
+                    <td>{((batsman2.runs / batsman2.balls) *100).toFixed(2)}</td>
+                    </tr>
                 </tbody>
+                
             </table>
             <div>
                 <button className={styles.button} onClick={() => alert(batsman.innings)}>
--- src/features/bats/batsmanSlice.ts
+++ src/features/bats/batsmanSlice.ts
@@ -5,12 +5,14 @@ import { Batsman } from "./interface/batsman";
 import { createNewBatsman } from "./helpers/createNewBatsman";
 
 export interface BatsmanState {
-    batsmen: Batsman;
+    onStrikeBatsmen: Batsman;
+    offStrikeBatsman: Batsman;
     status: 'idle' | 'loading' | 'failed';
 }
 
 const initialState: BatsmanState = {
-    batsmen: createNewBatsman(),
+    onStrikeBatsmen: createNewBatsman(),
+    offStrikeBatsman: createNewBatsman(),
     status: 'idle',
 };
 
@@ -19,28 +21,39 @@ export const batsmanSlice = createSlice({
     initialState,
     reducers: {
         addBatsmanName: (state, action: PayloadAction<string>) => {
-            state.batsmen.name = action.payload;
+            if (state.onStrikeBatsmen.name === '') {
+                state.onStrikeBatsmen.name = action.payload;
+                return;
+            }
+            else state.offStrikeBatsman.name = action.payload;
         },
         addRunsToBatsman: (state, action: PayloadAction<number>) => {
-            state.batsmen.runs += action.payload;
-            state.batsmen.balls += 1;
+            const currentbatsman = state.onStrikeBatsmen;
+            currentbatsman.runs += action.payload;
+            currentbatsman.balls += 1;
             if (action.payload === 4) {
-                state.batsmen.fours += 1;
+                currentbatsman.fours += 1;
             }
             if (action.payload === 6) {
-                state.batsmen.sixes += 1;
+                currentbatsman.sixes += 1;
             }
-            state.batsmen.innings.push(action.payload);
-        }
+            currentbatsman.innings.push(action.payload);
+        },
+        changeStrike: (state) => {
+            const temp = state.onStrikeBatsmen;
+            state.onStrikeBatsmen = state.offStrikeBatsman;
+            state.offStrikeBatsman = temp;
+        },
     }
 });
 
-export const { addBatsmanName, addRunsToBatsman } = batsmanSlice.actions;
+export const { addBatsmanName, addRunsToBatsman, changeStrike } = batsmanSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectBat = (state: RootState) => state.batsman.batsmen;
+export const selectBat = (state: RootState) => state.batsman.onStrikeBatsmen;
+export const selectBat2 = (state: RootState) => state.batsman.offStrikeBatsman;
 
 export default batsmanSlice.reducer;
 
